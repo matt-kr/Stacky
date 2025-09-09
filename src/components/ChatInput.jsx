cac597e70fcc25a72c03b41d878f202f765cd476
matt-kr <runnermatt07@hotmail.com>
2025-09-09 12:59:07 -0700

    better camera button

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -44,12 +44,14 @@ const handleSubmit = (e) => {
           onClick={onPhotoClick}
           disabled={isLoading}
         >
-          +
+          <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9 2C8.45 2 8 2.45 8 3V4H4C2.9 4 2 4.9 2 6V18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6C22 4.9 21.1 4 20 4H16V3C16 2.45 15.55 2 15 2H9ZM12 7C14.76 7 17 9.24 17 12C17 14.76 14.76 17 12 17C9.24 17 7 14.76 7 12C7 9.24 9.24 7 12 7ZM12 9C10.35 9 9 10.35 9 12C9 13.65 10.35 15 12 15C13.65 15 15 13.65 15 12C15 10.35 13.65 9 12 9Z" fill="currentColor"/>
+          </svg>
         </button>
         {showPhotoMenu && (
           <div className={`photo-menu ${isPhotoMenuClosing ? 'closing' : ''}`}>
             <button type="button" onClick={onPhotoUpload}>📷 Camera</button>
-            <button type="button" onClick={onCameraCapture}>🖼️ Photo</button>
+            <button type="button" onClick={onCameraCapture}>↑ Upload</button>
           </div>
         )}
       </div>
