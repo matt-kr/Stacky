630456960b3510cab9441b65b2124450662b8af3
matt-kr <runnermatt07@hotmail.com>
2025-09-09 22:51:57 -0700

    switched to gpt-5-nano for cost savings

--- api/reply.js
+++ api/reply.js
@@ -87,7 +87,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        model: 'gpt-4o',
+        model: 'gpt-5-nano',
         messages: messages,
         max_tokens: 1000,
         temperature: 0.7,
