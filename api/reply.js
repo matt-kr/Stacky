c071ef963325e55c6b9b893079c792d96e91b521
matt-kr <runnermatt07@hotmail.com>
2025-09-09 23:04:03 -0700

    back to 4o mini

--- api/reply.js
+++ api/reply.js
@@ -87,7 +87,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        model: 'gpt-5-thinking-nano',
+        model: 'gpt-4o-mini',
         messages: messages,
         max_tokens: 1000,
         temperature: 0.7,
